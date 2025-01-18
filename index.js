c401c9e1983cce7454df182f98280aa6042adf3b
Dahlberg94 <Marcus_dahlberg_94@hotmail.com>
2025-01-18 14:48:31 +0100

    test3

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-import {getProducts, getProduct} from "./services/api.js";
-
-import { createOrder, retrieveOrder } from "./services/klarna.js";
-import express from "express";
+import { getProducts, getProduct } from './services/api.js';
+import { createOrder, retrieveOrder } from './services/klarna.js';
+import express from 'express';
 const app = express();
-import { config } from "dotenv";
+import { config } from 'dotenv';
 config();
 
-
-app.get("/", async (req,res) => {
-    const products = await getProducts();
-    console.log(products);
-    const markup = products.map((p) =>`<a style="display: block; color: black; border: solid 2px black; margin:20px; padding: 5px;" href="/products/${p.id}">${p.title} - ${p.price} kr</a>`)
-    .join(' ');
-    res.send(markup);
+app.get('/', async (req, res) => {
+	const products = await getProducts();
+	// Lägga till vilken styling som helst nedan
+	const markup = products
+		.map(
+			(p) =>
+				`<a style="display:block;color:black;border:solid black 2px;margin: 20px; padding:10px;" href="/product/${p.id}">
+                    ${p.title} - ${p.price}kr
+                </a>`
+		)
+		.join(' ');
+	res.send(markup);
 });
 
-app.get("/products/:id", async (req, res) => {
-    try{
-        const { id } = req.params;
-        const product = await getProduct(id);
-        const klarnaResponse = await createOrder(product);
-        const markup = klarnaResponse.html_snippet;
-        res.send(markup);
-    } catch (error) {
-        res.send(error.message);
-    }
+app.get('/product/:id', async function (req, res) {
+	try {
+		const { id } = req.params;
+		const product = await getProduct(id);
+		const klarnaJsonResponse = await createOrder(product);
+		const html_snippet = klarnaJsonResponse.html_snippet;
+		res.send(html_snippet);
+	} catch (error) {
+		res.send(error.message);
+	}
 });
 
 app.get('/confirmation', async function (req, res) {
@@ -33,7 +37,5 @@ app.get('/confirmation', async function (req, res) {
 	const html_snippet = klarnaJsonResponse.html_snippet;
 	res.send(html_snippet);
 });
-
-
 
 app.listen(process.env.PORT);
--- services/api/api.js
+++
@@ -1,8 +0,0 @@
-import fetch from "node-fetch";
-
-export async function getProducts() {
-    const res = await fetch(`${process.env.FAKE_STORE_API_URL}/products`);
-    const products = await res.json();
-    return products;
-}
-
